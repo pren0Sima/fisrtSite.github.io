29b5b24b80fc9236076a6ca4b973f4dd9d84c602
pren0Sima <108150997+pren0Sima@users.noreply.github.com>
2023-03-27 14:02:37 +0300

    Add files via upload

---
+++ eventNotification.js
@@ -0,0 +1,5 @@
+const button = document.querySelector(" .btn");
+
+button.addEventListener('click', function() {
+  alert("Съобщението Ви няма да бъде доставено. Можете да се свържете с мен на следния имейл: simona.velichkova02@abv.bg");
+});
